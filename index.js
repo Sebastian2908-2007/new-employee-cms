c230b42a0bc7e146c2a5a0c3ad44b13115e390c7
Sebabin <sebastiancrespin279@gmail.com>
2022-01-12 14:57:12 -0700

    have method in Update class to update an employees role

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const View = require('./lib/View');
 const Add = require('./lib/Add');
+const Update = require('./lib/Update');
 
 
 
-new View().employees();
+
 //new Add().departments(dept_name);
+
+const role_id = 2;
+const emp_id = 18;
+// bill roberts original role id 6
+
+new Update().empRole(role_id,emp_id);
+new View().employees();
