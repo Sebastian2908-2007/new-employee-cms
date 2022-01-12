1f970cbfb6917c2438a9adb9ca8c0231bb8b34fa
Sebabin <sebastiancrespin279@gmail.com>
2022-01-11 17:30:13 -0700

    have method to add a new role

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const View = require('./lib/View');
 const Add = require('./lib/Add');
 
-const dept_name = "Tractor Depatrtment"
+//const dept_name = "Tractor Depatrtment"
 
-new View().employees();
-new Add().departments(dept_name);
+//new View().employees();
+//new Add().departments(dept_name);
+
+const title = 'Tractor Driver';
+const salary = 60000;
+const dept_id = 6;
+
+new Add().roles(title,salary,dept_id);
