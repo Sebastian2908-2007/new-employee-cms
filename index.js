dd201f5f2fc2590e499d0b7191f5dd655af975e7
Sebabin <sebastiancrespin279@gmail.com>
2022-01-11 17:54:20 -0700

    now have an method to add employees with the Add class

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const Add = require('./lib/Add');
 //new View().employees();
 //new Add().departments(dept_name);
 
-const title = 'Tractor Driver';
+/*const title = 'Tractor Driver';
 const salary = 60000;
 const dept_id = 6;
 
-new Add().roles(title,salary,dept_id);
+new Add().roles(title,salary,dept_id);*/
+
+const first_name = "Alastaire";
+const last_name = "Bowen";
+const role_id = 16;
+const manager_id = 29;
+
+new Add().employees(first_name,last_name,role_id,manager_id);
