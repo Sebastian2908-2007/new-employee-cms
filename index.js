079df0ba19a25fd8a1fdf153c7dfb61098934111
Sebabin <sebastiancrespin279@gmail.com>
2022-01-12 15:08:51 -0700

    have Update method to update employees manager

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const Update = require('./lib/Update');
 
 //new Add().departments(dept_name);
 
-const role_id = 2;
+const manger_id = 1;
 const emp_id = 18;
 // bill roberts original role id 6
 
-new Update().empRole(role_id,emp_id);
+new Update().empManager(manger_id,emp_id);
 new View().employees();
