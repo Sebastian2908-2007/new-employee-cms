d720e1366e1a15b72a6d8adfaf66d2ac08502c1a
Sebabin <sebastiancrespin279@gmail.com>
2022-01-11 18:30:49 -0700

    have roles join query executing and displaying properly in console

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 const View = require('./lib/View');
 const Add = require('./lib/Add');
 
-//const dept_name = "Tractor Depatrtment"
 
-//new View().employees();
+
+new View().roles();
 //new Add().departments(dept_name);
-
-/*const title = 'Tractor Driver';
-const salary = 60000;
-const dept_id = 6;
-
-new Add().roles(title,salary,dept_id);*/
-
-const first_name = "Alastaire";
-const last_name = "Bowen";
-const role_id = 16;
-const manager_id = 29;
-
-new Add().employees(first_name,last_name,role_id,manager_id);
