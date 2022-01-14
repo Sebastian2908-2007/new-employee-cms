58b07dca38a8e258cce56aaad2d652b7e313d459
Sebabin <sebastiancrespin279@gmail.com>
2022-01-13 17:37:21 -0700

    can now view all departments,roles and employees using cl prompt

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-const View = require('./lib/View');
-const Add = require('./lib/Add');
-const Update = require('./lib/Update');
-const Delete = require('./lib/Delete');
-const ViewBy = require('./lib/ViewBy');
+const Program = require('./lib/Program');
 
-
-
-
-//new Add().departments(dept_name);
-
-
-const role_id = 2;
-
-
-// 35 through 37 have roll 11
-
-//new Update().roleSalary(new_salary,role_id);
-//new Delete().employee(emp_id);
-//new View().employees();
-//new View().roles();
-//new ViewBy().manager(manager_id);
-new ViewBy().role(role_id);
+new Program().initializeProgram();
