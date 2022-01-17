ff9eb21e00cd4af05ba67eaa086c0b818646fea9
Sebabin <sebastiancrespin279@gmail.com>
2022-01-16 18:58:54 -0700

    have all post routes ready to go!

--- routes/apiRoutes/employee.js
+++ routes/apiRoutes/employee.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const View = require('../../lib/View');
+const Add = require('../../lib/Add');
 const allEmployees = new View().employees();
 
+// add an employee
+router.post('/employees',({body},res) => {
+    new Add().employees(body.first_name,body.last_name,body.role_id,body.manager_id);
+    res.json({
+        message:'success',
+        data: body
+    })
+});
+
+// get all employees
 router.get('/employees',(req,res) => {
     res.json({
         message:'success',
--- routes/apiRoutes/role.js
+++ routes/apiRoutes/role.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const View = require('../../lib/View');
+const Add = require('../../lib/Add');
 const allRoles = new View().roles();
 
+// add a role
+router.post('/roles',({body},res) => {
+    new Add().roles(body.title,body.salary,body.department_id);
+
+    res.json({
+        message:'success',
+        data: body
+    })
+});
+
+// get all roles
 router.get('/roles',(req,res) => {
     res.json({
         message:'success',
