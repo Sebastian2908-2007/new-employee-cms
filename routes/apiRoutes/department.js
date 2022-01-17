8f26da488978768450efb75700fb64d83e04e8f2
Sebabin <sebastiancrespin279@gmail.com>
2022-01-16 18:30:37 -0700

    can now post a new department

--- routes/apiRoutes/department.js
+++ routes/apiRoutes/department.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const View = require('../../lib/View');
+const Add = require('../../lib/Add')
 const allDept = new View().departments();
 
 
+router.post('/departments',({body},res) => {
+    new Add().departments(body.dept_name)
+     res.json({
+         message:'success',
+         data: body
+     })
+});
+
+// get departments
 router.get('/departments',(req,res) => {
    
     res.json({
